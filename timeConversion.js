7c742a497a532f48e42737b9bd75eaf1cd46dfe8
YousufIslamRathore <rathoreislam@gmail.com>
2023-01-18 05:09:10 +0500

    Updating Code

--- timeConversion.js
+++ timeConversion.js
@@ -1,11 +1,15 @@
 // Given Input
-let s = '12:05:45AM';
+let s = "12:05:45PM";
 
 let hrs = s.slice(0, 2);
-let timeSpan = s.slice(-2)
+let timeSpan = s.slice(-2);
 
-if(timeSpan == "AM") {
-    hrs = Nubmer(hrs) - 12
+if (timeSpan == "AM" && +hrs >= 12) {
+  hrs = "0" + (+hrs - 12);
+} else if (timeSpan == "PM" && +hrs < 12) {
+  hrs = +hrs + 12;
 }
 
-console.log(hrs)
+let resultTime = hrs + s.slice(2, -2);
+
+console.log(resultTime);
