27a563f4099579d37ea7ae2c584b81d32b7f367d
YousufIslamRathore <rathoreislam@gmail.com>
2024-06-03 19:50:15 +0500

    pushing minor update before going full on this hackerrank

--- gradingStudents.js
+++ gradingStudents.js
@@ -1,11 +1,17 @@
-let grade;
-    let updatedGrades = [];
-    for (let i = 0; i < grades.length; i++) {
-        grade = grades[i];
-        for (let j = 0; j < 2; j++) {
-            if (grade >= 38 && grade % 5 >= 3) {
-                grade++
-            }
-        }
-        updatedGrades.push(grade)
+// Given Input
+let grades = [73, 67, 38, 33]; 
+
+let grade;                  // Initiated for storing ith iteration of grades
+let updatedGrades = [];     // For pushing in the updated grade values
+for (let i = 0; i < grades.length; i++) {
+  grade = grades[i];
+  for (let j = 0; j < 2; j++) {
+    // Loop made for checking the grades till it is becoming the multiple of 5
+    if (grade >= 38 && grade % 5 >= 3) {
+      grade++;
     }
+  }
+  updatedGrades.push(grade); // pushing the grade value into the array after performing conditional function
+}
+
+console.log(updatedGrades)
